735f10f5ee4569e5e72f123de69534e240853ac3
IvanOrozco-nw <jorozco@netwarmonitor.com>
2019-11-01 20:34:13 -0600

    Correccion tf2

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.setPresence( {
+client.user.setPresence( {
   game:{
     name:"Team Fortress 2"
   } 
